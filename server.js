6d24103d15548a3c72e31cfd179a5829ec3f8867
aliitz <alseus13@hotmail.com>
2021-03-19 16:32:48 +0700

    debug

--- server.js
+++ server.js
@@ -12,20 +12,11 @@ app.use(bodyParser.json())
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
-app.use(bodyParser.json())
-
 app.post("/", (req, res) => {
   const body = {
     challenge : req.body.challenge
   }
   res.send(body)
   console.log(req.body) // Call your action on the request here
-  console.log( {"challenge": JSON.parse(event.body).challenge});
-  const response = {
-    statusCode: 200,
-    body: event.body
-    
-  };
-  console.log(response);
   res.status(200).end() // Responding is important
 })
